b1f670410765c0f2b73064d5542066d42e2a707e
Drew Reynolds <drew.reynolds@gramercytech.com>
2016-04-18 13:15:12 -0400

    Add First & Last Name fields to User. Add Valid User Factory

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,17 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+  devise :database_authenticatable,
+         :registerable,
+         :recoverable,
+         :rememberable,
+         :trackable,
+         :validatable
+
+  validates :first_name,length: { minimum: 2 }, format: { with: /\A(?=.*[a-z])[a-z\d]+\Z/i }
+  validates :last_name, length: { minimum: 2 }, format: { with: /\A(?=.*[a-z])[a-z\d]+\Z/i }
+
+  validates_presence_of :first_name,
+                        :last_name,
+                        :email
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
+require 'factory_girl_rails'
 
-RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe User do
+
+  it "has a valid factory" do
+    user = FactoryGirl.build(:user)
+    expect(user).to be_valid
+  end
+
 end
