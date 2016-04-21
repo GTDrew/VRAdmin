abaf3eff7143a56baa905c66412278b0f5432d5a
Drew Reynolds <drew.reynolds@gramercytech.com>
2016-04-21 10:47:50 -0400

    Create sidebar layout and dashboard index view

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'welcome#index'
-
+  resources :dashboard
   #   resources :products
   get '/users/:id', to: 'users#show', as: 'user'
 
