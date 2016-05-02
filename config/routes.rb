78be00aab88217c5496c30fce3ca075e4ab0482f
Drew Reynolds <drew.reynolds@gramercytech.com>
2016-05-02 14:37:17 -0400

    Add Administrate & Admin boolean to User model

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
+
+  namespace :admin do
+    resources :users
+    resources :apps
+    resources :videos
+
+    root to: "users#index"
+  end
+
   devise_for :users
   root 'welcome#index'
 
