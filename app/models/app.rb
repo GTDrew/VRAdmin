849b015f7703d63f0a2f172c666b80d7139aee4e
Drew Reynolds <drew.reynolds@gramercytech.com>
2016-05-09 10:17:11 -0400

    Able to download assets as zip with correct naming conventions

--- app/models/app.rb
+++ app/models/app.rb
@@ -1,16 +1,18 @@
 class App < ActiveRecord::Base
-    belongs_to :owner, :class_name => "User", :foreign_key => "user_id"
+  belongs_to :owner, :class_name => "User", :foreign_key => "user_id"
 
-    has_many :videos
+  has_many :videos
 
-    validates :background_color, length: { is: 7 }
-    validates :font_color, length: { is: 7 }
+  validates :background_color, length: { is: 7 }
+  validates :font_color, length: { is: 7 }
 
-    validates :name, presence: true,
-                     uniqueness: true,
-                     length: { maximum: 12 }
+  validates :name,
+            presence: true,
+            uniqueness: true,
+            length: { maximum: 12 }
 
-    mount_uploader :icon, AppIconUploader
-    mount_uploader :header_image, AppHeaderUploader
-    mount_uploader :splash_image, AppSplashUploader
+  mount_uploader :icon, AppIconUploader
+  mount_uploader :header_image, AppHeaderUploader
+  mount_uploader :splash_image, AppSplashUploader
+
 end
