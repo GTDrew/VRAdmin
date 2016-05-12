f57e94c5598d73965b1b980d05d0c56adb486053
Drew Reynolds <drew.reynolds@gramercytech.com>
2016-05-12 11:20:10 -0400

    Fix spacing

--- app/controllers/apps_controller.rb
+++ app/controllers/apps_controller.rb
@@ -1,103 +1,102 @@
 class AppsController < ApplicationController
-  require 'rubygems'
-  require 'zip'
-  require 'open-uri'
-  layout "app"
+ require 'rubygems'
+ require 'zip'
+ require 'open-uri'
+ layout "app"
 
-  before_action :authenticate_user!
-  before_action :require_owner, :except => [:new, :create]
+ before_action :authenticate_user!
+ before_action :require_owner, :except => [:new, :create]
 
-  def show
-    @app = App.find(params[:id])
+ def show
+  @app = App.find(params[:id])
 
-    respond_to do |format|
-      format.html
-      format.zip do
+  respond_to do |format|
+   format.html
+   format.zip do
+    compressed_filestream = Zip::OutputStream.write_buffer do |zos|
 
-        compressed_filestream = Zip::OutputStream.write_buffer do |zos|
+     zos.put_next_entry "title_icon.png"
+     zos.write open(@app.header_image.url).read
 
-          zos.put_next_entry "title_icon.png"
-          zos.write open(@app.header_image.url).read
+     zos.put_next_entry "ic_launcher.png"
+     zos.write open(@app.icon.url).read
 
-          zos.put_next_entry "ic_launcher.png"
-          zos.write open(@app.icon.url).read
-
-          zos.put_next_entry "splash_icon.png"
-          zos.write open(@app.splash_image.url).read
-        end
-        compressed_filestream.rewind
-        send_data compressed_filestream.read, filename: "#{@app.name}-#{@app.id}.zip"
-      end
+     zos.put_next_entry "splash_icon.png"
+     zos.write open(@app.splash_image.url).read
     end
+    compressed_filestream.rewind
+    send_data compressed_filestream.read, filename: "#{@app.name}-#{@app.id}.zip"
+   end
   end
+ end
 
-  def new
-    @app = App.new
+ def new
+  @app = App.new
+ end
+
+ def edit
+  @app = App.find(params[:id])
+ end
+
+ def create
+  @app = App.new(app_params)
+  @app.user_id = current_user.id
+
+  if @app.save
+   flash[:notice] = 'App successfully created!'
+   redirect_to app_path(@app)
+  else
+   flash[:error] = @app.errors.full_messages.join(', ')
+   render 'new'
   end
+ end
 
-  def edit
-    @app = App.find(params[:id])
+ def update
+  @app = App.find(params[:id])
+
+  if @app.update(app_params)
+   flash[:notice] = 'App successfully updated!'
+   redirect_to @app
+  else
+   flash[:error] = @app.errors.full_messages.join(', ')
+   render 'edit'
   end
+ end
 
-  def create
-    @app = App.new(app_params)
-    @app.user_id = current_user.id
+ def destroy
+  @app = App.find(params[:id])
+  @app.destroy
+  redirect_to apps_path
+ end
 
-    if @app.save
-      flash[:notice] = 'App successfully created!'
-      redirect_to app_path(@app)
-    else
-      flash[:error] = @app.errors.full_messages.join(', ')
-      render 'new'
-    end
-  end
+ def preview_iphone
+  @app = App.find(params[:id])
+  @videos = @app.videos
+  @video = @videos.first
+ end
 
-  def update
-    @app = App.find(params[:id])
+ def preview_android
+  @app = App.find(params[:id])
+  @videos = @app.videos
+  @video = @videos.first
+ end
 
-    if @app.update(app_params)
-      flash[:notice] = 'App successfully updated!'
-      redirect_to @app
-    else
-      flash[:error] = @app.errors.full_messages.join(', ')
-      render 'edit'
-    end
-  end
+ def submit
+  @app = App.find(params[:id])
+ end
 
-  def destroy
-    @app = App.find(params[:id])
-    @app.destroy
-    redirect_to apps_path
-  end
-
-  def preview_iphone
-    @app = App.find(params[:id])
-    @videos = @app.videos
-    @video = @videos.first
-  end
-
-  def preview_android
-    @app = App.find(params[:id])
-    @videos = @app.videos
-    @video = @videos.first
-  end
-
-  def submit
-    @app = App.find(params[:id])
-  end
-
-  private
-  
-  def app_params
-    params.require(:app).permit(
-      :name,
-      :icon,
-      :header_image,
-      :splash_image,
-      :background_color,
-      :font_color,
-      :splash_color,
-      :submitted
-      )
-  end
+ private
+ 
+ def app_params
+  params.require(:app).permit(
+   :name,
+   :icon,
+   :header_image,
+   :splash_image,
+   :background_color,
+   :font_color,
+   :splash_color,
+   :submitted
+   )
+ end
 end
