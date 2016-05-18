bd85b77217e584e22a679f3b59992e182688ef29
Drew Reynolds <drew.reynolds@gramercytech.com>
2016-05-18 15:14:12 -0400

    Add created_at to video filename incase 2 videos with same name are uploaded

--- app/uploaders/video_uploader.rb
+++ app/uploaders/video_uploader.rb
@@ -45,10 +45,8 @@ class VideoUploader < CarrierWave::Uploader::Base
 
   # Override the filename of the uploaded files:
   # Avoid using model.id or version_name here, see uploader/store.rb for details.
-  # def filename
-  #   "something.jpg" if original_filename
-  # end
-
-
+  def filename
+   "#{model.name}-#{model.created_at.strftime('%m-%e-%y-%H-%M-%S')}.mp4"
+  end
 
 end
